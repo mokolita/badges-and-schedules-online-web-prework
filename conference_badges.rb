e71597f9613a7ec0b798842658fad8cbd83d107b
Meg Galvez <meg@oakmaine.com>
2019-02-28 11:38:40 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,14 @@
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
+end
+
+def assign_room(array)
+  
